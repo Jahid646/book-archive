612e8338fef4c31b73a7d98a45a07e6823137581
Jahid Hasan <jhasan646@gmail.com>
2021-09-02 18:39:05 +0600

    add total search result

--- app.js
+++ app.js
@@ -13,18 +13,28 @@ searchBook = () => {
 
     fetch(url)
     .then(res => res.json())
-    .then(data =>  displaySearchResult(data.docs));
+    .then(data =>  displaySearchResult(data.docs, data.numFound));
+    
     }
 }
 
+
+
 // dilsplay search result
-const displaySearchResult = docs => {
+const displaySearchResult = (docs, numFound) => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
     if(docs.length === 0){
         document.getElementById('error-message').style.display = 'block';
     }
 
+    // total result found 
+    const div  = document.getElementById('total-result');
+    div.innerHTML = `
+        <div class="text-danger">
+              <h5>Showing ${docs.length} Result of ${numFound}</h5>
+        </div>`;
+    
 
 
     docs.forEach(doc => {
@@ -32,13 +42,16 @@ const displaySearchResult = docs => {
         div.classList.add('col');
         div.innerHTML = `
             <div class="card h-100">
-                <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top img-fluid h-100" alt="...">
+                <img height="500px" src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Book Name: ${doc.title}</h5>
-                  <p class="card-text">Author Name: ${doc.author_name}</p>
-                  <p class="card-text">Publisher: ${doc.publisher}</p>
+                  <p class="card-text">Author: ${doc.author_name}</p>
                   <p class="card-text">First Publish: ${doc.first_publish_year}</p>
+                  <p class="card-text">Publisher: ${doc.publisher}</p>
             </div>`;
         searchResult.appendChild(div);
+        for(count=0; count<100; count++){
+
+        }
     })
 }
